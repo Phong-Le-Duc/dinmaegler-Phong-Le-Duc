ff75c20fd4728131c3b50358cfd326e4354c3533
Phong-Le-Duc <phongleduc@gmail.com>
2025-09-03 14:02:39 +0200

    add login works

--- src/components/layout/header/Topbar.tsx
+++ src/components/layout/header/Topbar.tsx
@@ -1,14 +1,18 @@
-import mailto from "/src/assets/mail_icon.svg"
+import mailto from "/src/assets/mail_icon.svg";
 import phoneIcon from "/src/assets/phone_icon.svg";
 import loginIcon from "/src/assets/login_icon.svg";
 import { Link } from "react-router-dom";
-
+import { useAuth } from "../../../contexts/AuthContext";
 
 export default function Topbar() {
+    const auth = useAuth();
+    const token = auth?.token;
+    const logout = auth?.logout;
+
     return (
         <>
             <div className="bg-dinmaegler-blue">
-                <div className="header-width flex items-center justify-between  p-5 gap-4">
+                <div className="header-width flex items-center justify-between p-5 gap-4">
                     <a
                         href="mailto:4000@dinmaegler.com"
                         className="flex items-center text-white hover:underline"
@@ -22,9 +26,23 @@ export default function Topbar() {
                         +45 7070 4000
                     </a>
 
-                    <Link to="/login" className="flex items-center ml-auto"><img src={loginIcon} alt="login" /><p className="text-white">Log in</p></Link>
+                    {token ? (
+                        <button onClick={logout} className="flex items-center ml-auto cursor-pointer bg-black p-2 rounded">
+                            <img src={loginIcon}
+                                alt="logout"
+                                className="mr-2" />
+                            <p className="text-white">Log ud</p>
+                        </button>
+                    ) : (
+                        <Link to="/login" className="flex items-center ml-auto cursor-pointer bg-black p-2 rounded">
+                            <img src={loginIcon}
+                                alt="login"
+                                className="mr-2" />
+                            <p className="text-white">Log in</p>
+                        </Link>
+                    )}
                 </div>
             </div>
         </>
-    )
+    );
 }
