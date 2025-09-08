90b61eb572e6869b155c15b8479b3c3b37b4d83d
Phong-Le-Duc <phongleduc@gmail.com>
2025-09-08 13:14:00 +0200

    hold dig opdateret færdig

--- src/components/sections/HoldDigOpdateretSection.tsx
+++ src/components/sections/HoldDigOpdateretSection.tsx
@@ -1,24 +1,34 @@
-export default function HoldDigOpdateretSection () {
+export default function HoldDigOpdateretSection() {
 
-return (
-       <section className="bg-dinmaegler-blue flex items-center justify-center relative">
-                <div className="content-width flex py-10">
-                    <div>
-                        <div className="text-white">
-                            <h2 className="h2">Hold dig opdateret
-                                på salgsprocessen</h2>
-                            <p>Når du sælger din bolig hos Din Mægler, kommunikerer du nemt med den ansvarlige mægler eller butik med vores app. Her kan du også se statistik på interessen for din bolig i alle vores salgskanaler.</p>
-                        </div>
-                        <div className="flex gap-3 mt-5">
-                            <button className="bg-dinmaegler-white flex px-5 py-3"><img src="src/assets/play-store.png" alt="Google Play" className="w-4 h-4" /><p>Google Play</p></button>
-                            <button className="bg-transparent border border-white py-3"><img className="w-4 h-4" src="src/assets/apple.png" alt="Apple Store" /><p className="text-white">Apple Store</p></button>
-                        </div>
+    return (
+        <section className="bg-dinmaegler-blue flex items-center justify-center relative">
+            <div className="content-width flex py-15 relative">
+                <div>
+                    <div className="text-white">
+                        <h2 className="h2 mb-2">Hold dig opdateret
+                            på salgsprocessen</h2>
+                        <p className="max-w-100">Når du sælger din bolig hos Din Mægler, kommunikerer du nemt med den  ansvarlige mægler eller butik med vores app. Her kan du også se statistik på <br /> interessen for din bolig i alle vores salgskanaler.</p>
                     </div>
-                    <div className="flex">
-                        <figure><img src="src/assets/phone1.png" alt="" /></figure>
-                        <figure><img src="src/assets/phone2.png" alt="" /></figure>
+                    <div className="flex gap-3 mt-5">
+                        <button className="bg-dinmaegler-white flex items-center gap-2 px-5 py-3 rounded shadow hover:bg-gray-100 transition">
+                            <img src="src/assets/play-store.png" alt="Google Play" className="w-5 h-5" />
+                            <span className="font-medium text-dinmaegler-blue">Google Play</span>
+                        </button>
+                        <button className="bg-transparent border border-white flex items-center gap-2 px-5 py-3 rounded hover:bg-white hover:text-dinmaegler-blue transition">
+                            <img src="src/assets/apple.png" alt="Apple Store" className="w-5 h-5" />
+                            <span className="text-white font-medium group-hover:text-dinmaegler-blue">Apple Store</span>
+                        </button>
+                    </div>
+                    <div className="flex items-end ml-8 absolute right-12 bottom-0">
+                        <figure className="z-10">
+                            <img src="src/assets/phone1.png" alt="App på telefon 1" className="h-65 w-auto object-contain" />
+                        </figure>
+                        <figure className="-ml-16 z-0">
+                            <img src="src/assets/phone2.png" alt="App på telefon 2" className="h-65 w-auto object-contain" />
+                        </figure>
                     </div>
                 </div>
-            </section>
-)
+            </div>
+        </section>
+    )
 }
