ef1caca2debfc00895dbf820b6f4ec7a23b96ccd
Phong-Le-Duc <phongleduc@gmail.com>
2026-09-01 13:09:56 +0200

    GalleryHouse: use full-width on mobile with 16px gutters (w-[calc(100%-32px)]) to fix left alignment

--- src/components/common/GalleryHouse.tsx
+++ src/components/common/GalleryHouse.tsx
@@ -84,7 +84,7 @@ export default function GalleryHouse({
 
     return (
         <dialog
-            className="backdrop:bg-black backdrop:opacity-75 mx-auto my-4 w-full max-w-[40rem] h-auto max-h-[80vh] bg-transparent border-0"
+            className="backdrop:bg-black backdrop:opacity-75 w-[calc(100%-32px)] md:w-full md:max-w-[40rem] mx-auto my-4 h-auto max-h-[80vh] bg-transparent border-0"
             ref={dialogRef}
         >
             <button
